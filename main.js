16b13730a4a5e2fb799d55c2236dd8be778a2070
fazlulhoque <fazlul.bba96@gmail.com>
2024-01-06 17:26:32 +0600

    init

---
+++ main.js
@@ -0,0 +1,23 @@
+const todoBox = document.querySelector("#todo-box");
+const addBtn = document.querySelector("#add");
+const deleteBtn = document.querySelector("#delete");
+const editBtn = document.querySelector("#edit");
+
+
+const todoList = [
+  {
+    id: 1,
+    title: "Complete Javascript",
+    isChecked: false
+  },
+  {
+    id: 2,
+    title: "Complete Todo Application",
+    isChecked: false
+  }
+]
+
+
+// add new todo
+
+
